c5023291712e81cb8ee72d4bc93041e24fc1bd1d
David Björklund <david.bjorklund@gmail.com>
2014-07-12 14:49:34 +0200

    work with words that are minor in a title

--- name-extractor.js
+++ name-extractor.js
@@ -1,4 +1,9 @@
-var Extractor = function (input) {
+var minors = require('title-case-minors').reduce(function (obj, minor) {
+      obj[minor] = true
+      return obj
+    }, {})
+
+  , Extractor = function (input) {
       this.tokens = input.split(' ')
       this.names = []
     },
@@ -6,13 +11,33 @@ var Extractor = function (input) {
       return string[0] >= 'A' && string[0] <= 'Z'
     }
 
+Extractor.prototype._findNamesWithMinor = function (startIndex) {
+  var index = startIndex + 1
+
+  // first, step to the capitalized word
+  while((token = this.tokens[index]) && minors[token])
+    index++
+
+  // second, add all the words that are capitalized or minors
+  while((token = this.tokens[index]) && (isCapitalized(token) || minors[token])) {
+    this.names.push(this.tokens.slice(startIndex, index + 1).join(' '))
+    index++
+  }
+}
+
 Extractor.prototype._findNames = function (startIndex) {
   var index = startIndex
     , token
 
-  while((token = this.tokens[index]) && isCapitalized(token)) {
+  while((token = this.tokens[index]) && (isCapitalized(token) || minors[token])) {
     this.names.push(this.tokens.slice(startIndex, index + 1).join(' '))
     index++
+  }
+
+  index = startIndex - 1
+  while((token = this.tokens[index]) && minors[token]) {
+    this._findNamesWithMinor(index)
+    index--
   }
 }
 
--- test.js
+++ test.js
@@ -1,13 +1,29 @@
 var test = require('tape'),
     extract = require('./name-extractor')
 
-test(function (t) {
+test('simple', function (t) {
   t.deepEqual(
       extract('beep Hello World boop').sort()
     , [
           'Hello'
         , 'Hello World'
         , 'World'
+      ]
+  )
+  t.end()
+})
+
+test('include title minor-case words', function (t) {
+  t.deepEqual(
+      extract('they were playing Crying at the Discoteque yesterday').sort()
+    , [
+          'Crying'
+        , 'Crying at'
+        , 'Crying at the'
+        , 'Crying at the Discoteque'
+        , 'Discoteque'
+        , 'at the Discoteque'
+        , 'the Discoteque'
       ]
   )
   t.end()
